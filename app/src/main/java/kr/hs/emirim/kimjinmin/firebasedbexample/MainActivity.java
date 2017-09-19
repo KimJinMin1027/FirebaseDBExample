132bb60ef49910a4dba2f7bb1e294e4f8c10fc82
김진민 <jinmin5161@gmail.com>
2017-09-19 19:58:44 +0900

    #2 뷰 참조 연결

--- app/src/main/java/kr/hs/emirim/kimjinmin/firebasedbexample/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kimjinmin/firebasedbexample/MainActivity.java
@@ -2,46 +2,23 @@ package kr.hs.emirim.kimjinmin.firebasedbexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    static final String TAG = "파이어베이스 : MainActivity";
+    private TextView mNameTextView; //멤버변수 이름 앞엔 m붙여서 보기 편하도록
+    private TextView mGithubTextView;
+    private ImageView mProfileImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
-
-
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-                Toast.makeText(getApplication(), value, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
+        mNameTextView=(TextView) findViewById(R.id.profile_name);
+        mGithubTextView=(TextView) findViewById(R.id.profile_github);
+        mProfileImageView=(ImageView) findViewById(R.id.profile_image);
+        
 
     }
 }
